ac2e21f1d484f878f3d52059d6138be1e46b6347
DIamond16617 <duckisuckjk@gmail.com>
2020-11-03 19:16:33 -0500

    Add files via upload

--- channels.js
+++ channels.js
@@ -3,18 +3,30 @@ xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var foo = JSON.parse(this.responseText);
     var i = 0;
+    f="";
     for (i = 0; i<32; i++){
     y = foo[i];
-    f+="\n<div class=\"item\">\n<a href=\"load("+y.videoId+")\">\n<div class=\"channel0\">\n<img src=\"https://i.ytimg.com/vi/"+y.videoId+"/hqdefault.jpg\"/>\n</div>\n<div class=\"sm-txt\">\n<h3>"+y.title+"</h3>\n</div>\n</a>\n</div>\n</div>\n";
+    f = `${f}
+<div class="item">
+<a onclick="load(${y.videoId});return false">
+<div class="channel0">
+<img src="https://i.ytimg.com/vi/${y.videoId}/hqdefault.jpg"/>
+</div>
+<div class="sm-txt">
+<h3>${y.title}</h3>
+</div>
+</a>
+</div>
+</div>
+`;
     }
     document.getElementById("container").innerHTML = f;
-    console.log(f);
-    
     
     
   }
 };
 
-xmlhttp.open("GET", "https://invidious.snopyta.org/api/v1/channels/UChFur_NwVSbUozOcF_F2kMg/videos?page=1", true);
-xmlhttp.setRequestHeader('Access-Control-Allow-Origin', 'https://invidious.snopyta.org');
+xmlhttp.open("GET", "https://tube.connect.cafe/api/v1/channels/UChFur_NwVSbUozOcF_F2kMg/videos?page=1", true);
+//xmlhttp.setRequestHeader('Access-Control-Allow-Origin', 'https://https://tube.connect.cafe/');
+//xmlhttp.setRequestHeader('referer', 'https://https://tube.connect.cafe/');
 xmlhttp.send();
