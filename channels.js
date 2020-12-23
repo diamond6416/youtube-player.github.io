b0ef80f20e8e10c3151291910a0d2fc149ee9dfb
DIamond16617 <duckisuckjk@gmail.com>
2020-12-23 18:15:51 -0500

    Add files via upload

--- channels.js
+++ channels.js
@@ -1,26 +1,16 @@
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    var z = JSON.parse(this.responseText);
+var f = "";
+let channelURL = "https://invidiou.site/api/v1/channels/UChFur_NwVSbUozOcF_F2kMg/videos?page=1";
+fetch (channelURL)
+.then (response0 => response0)
+.then (rJSON0 => rJSON0.json())
+.then (function(z){
+  for (j = 0; j < 20; j++) {
 
-    var f = "";
-    var j = 0;
-    for (j = 0; j < 20; j++) {
+    g = z[j];
 
-      g = z[j];
+    vidId = g.videoId;
+    ti = g.title;
 
-      vidId = g.videoId;
-      ti = g.title;
-
-      f += "\n<div class=\"item\">\n<a href=\"javascript:load(\'" + vidId + "\')\">\n<div class=\"channel0\">\n<img src=\"https://i.ytimg.com/vi/" + vidId + "/hqdefault.jpg\"/>\n</div>\n<div class=\"sm-txt\">\n<h3>" + ti + "</h3>\n</div>\n</a>\n</div>\n</div>\n";
-    }
-    document.getElementById("container").innerHTML = f;
-    
-
-
+    f += '\n<div class=\"item\">\n<a href=\"javascript:load(\'' + vidId + '\')\">\n<div class=\"channel0\">\n<img src=\"https://i.ytimg.com/vi/' + vidId + '/hqdefault.jpg\"/>\n</div>\n<div class=\"sm-txt\">\n<h3>' + ti + '</h3>\n</div>\n</a>\n</div>\n</div>\n';
   }
-};
-
-xmlhttp.open("GET", "https://invidiou.site/api/v1/channels/UChFur_NwVSbUozOcF_F2kMg/videos?page=1", true);
-//xmlhttp.setRequestHeader("Origin", "https://heroku.app/");
-xmlhttp.send();
+  document.getElementById("container").innerHTML = f;});
--- play.js
+++ play.js
@@ -1,12 +1,29 @@
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    var foo = JSON.parse(this.responseText);
-    x = foo.info;
+bool = true;
+fallbackvid = false;
+l = 21;
+function load(a){
+let auURL = "https://mz8pri1hgfmas0rsq5x.herokuapp.com/api/play?url=https://www.youtube.com/watch?v="+a+"&format=bestaudio";
+let vidUrlS = "https://mz8pri1hgfmas0rsq5x.herokuapp.com/api/info?url=https://www.youtube.com/watch?v="+a+"&flatten=True";
+
+fetch (vidUrlS)
+.then (response => response)
+.then (rJSON => rJSON.json())
+.then (function(foo){
+
+
+x = foo.videos[0];
+
     list = new Array(-1, -1, -1, -1, -1, -1);
     
     
-    for (i = 0; i<21; i++){
+    for (i = 0; (i<l && bool); i++){
+    
+   
+    if ( typeof x.formats[i] === 'undefined' || x.formats[i] === null ){
+      bool = false;
+    }
+
+  else {
     y = x.formats[i];
     if ((y.ext=="webm") && (y.format_note=="1080p")){list[0]=i;}
     else if ((y.ext=="webm") && (y.format_note=="1080p60")){list[1]=i;}
@@ -14,28 +31,30 @@ xmlhttp.onreadystatechange = function() {
     else if ((y.ext=="mp4") && (y.format_note=="1080p60")){list[3]=i;}
     else if ((y.ext=="webm") && (y.format_note=="720p")){list[4]=i;}
     else if ((y.ext=="webm") && (y.format_note=="720p60")){list[5]=i;}
-    }
-    
-    
-    if (list[0]>=0){f=x.formats[list[0]].url;}
-    else if (list[1]>=0){f=x.formats[list[1]].url;}
-    else if (list[2]>=0){f=x.formats[list[2]].url;}
-    else if (list[3]>=0){f=x.formats[list[3]].url;}
-    else if (list[4]>=0){f=x.formats[list[4]].url;}
-    else if (list[5]>=0){f=x.formats[list[5]].url;}
-    
-    console.log(f);
-
-    t = "<video class=\"myvideo\" id=\"myvideo\" controls muted>\n<source src=\""+f+"/>\n<audio id=\"myaudio\">\n<source src=\""+auUrl+"\"/>\n</audio>\n</video>";
-    document.getElementById("sample").innerHTML = t;
-    //document.getElementById("myvideo").src = f;
+    else {fallbackvid = true;}
   }
-};
-function load(a){
-auUrl = "https://main163ey8y3y713183134jfn24u.herokuapp.com/api/play?url=https://www.youtube.com/watch?v="+a+"&format=bestaudio";
-//document.getElementById("myaudio").src = auUrl;
-var vidUrl = "https://main163ey8y3y713183134jfn24u.herokuapp.com/api/info?url=https://www.youtube.com/watch?v="+a;
-xmlhttp.open("GET", vidUrl, true);
-//xmlhttp.setRequestHeader("Origin", "https://heroku.app/");
-xmlhttp.send();
-}
+    }
+    if (fallbackvid){const vidURL= "https://mz8pri1hgfmas0rsq5x.herokuapp.com/api/play?url=https://www.youtube.com/watch?v="+a+"&format=bestvideo";}
+    else {
+    if (list[0]>=0){f=x.formats[list[0]];}
+    else if (list[1]>=0){f=x.formats[list[1]];}
+    else if (list[2]>=0){f=x.formats[list[2]];}
+    else if (list[3]>=0){f=x.formats[list[3]];}
+    else if (list[4]>=0){f=x.formats[list[4]];}
+    else if (list[5]>=0){f=x.formats[list[5]];}
+    const vidURL = f.url;
+    }
+
+    var video = document.getElementById('myvideo');
+    var vsource = document.getElementById('svideo');
+    var audio = document.getElementById('myaudio');
+    var asource = document.getElementById('saudio');
+
+    vsource.src = vidURL;
+    asource.src = auURL;
+
+    video.load();
+    video.play();
+  }
+);
+  }
