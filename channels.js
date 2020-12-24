6b2197a42f3cfdc99c204fdddadd20ee4e535bc1
DIamond16617 <duckisuckjk@gmail.com>
2020-12-24 16:19:32 -0500

    Add files via upload

--- channels.js
+++ channels.js
@@ -1,6 +1,7 @@
 var f = "";
 var g = null;
-let channelURL = "https://invidiou.site/api/v1/channels/UChFur_NwVSbUozOcF_F2kMg/videos?page=1";
+var prefix = "tube.connect.cafe";
+const channelURL = "https://"+prefix+"/api/v1/channels/UChFur_NwVSbUozOcF_F2kMg/videos?page=1";
 fetch (channelURL)
 .then (response0 => response0)
 .then (rJSON0 => rJSON0.json())
@@ -12,6 +13,6 @@ fetch (channelURL)
     var vidId = g.videoId;
     var ti = g.title;
 
-    f += '\n<div class=\"item\">\n<a href=\"javascript:load(\'' + vidId + '\')\">\n<div class=\"channel0\">\n<img src=\"https://i.ytimg.com/vi/' + vidId + '/hqdefault.jpg\"/>\n</div>\n<div class=\"sm-txt\">\n<h3>' + ti + '</h3>\n</div>\n</a>\n</div>\n</div>\n';
+    f += '\n<div class=\"item\">\n<a class=\"activator\" onclick=\"load(\'' + vidId + '\')\">\n<div class=\"channel0\">\n<img src=\"https://i.ytimg.com/vi/' + vidId + '/hqdefault.jpg\"/>\n</div>\n<div class=\"sm-txt\">\n<h3>' + ti + '</h3>\n</div>\n</a>\n</div>\n</div>\n';
   }
   document.getElementById("container").innerHTML = f;});
