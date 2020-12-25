7ad219fba2466ed0043aee8a473f5ca3f3e0160f
DIamond16617 <22861081+diamond6416@users.noreply.github.com>
2020-12-24 22:52:32 -0500

    Add files via upload

--- dual.js
+++ dual.js
@@ -4,11 +4,13 @@ var myvideo = document.getElementById("myvideo");
     var change_time_state = true;
 
     myvideo.onplay = function(){
+        myvideo.oncanplay = function() {
         myaudio.play();
         if(change_time_state){
             myaudio.currentTime = myvideo.currentTime;
             change_time_state = false;
         }
+    }
     }
 
     myvideo.onpause = function(){
