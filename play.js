15f248e3387341a55a8cf48503d75c14d7df83db
DIamond16617 <22861081+diamond6416@users.noreply.github.com>
2020-12-24 23:42:28 -0500

    Add files via upload

--- play.js
+++ play.js
@@ -53,10 +53,50 @@ var x = foo.videos[0];
 
     
 
-    vsource.src = vidURL;
+    //vsource.src = vidURL;
     asource.src = auURL;
-    
 
+    // START
+    var assetURL = vidURL;
+      // Need to be specific for Blink regarding codecs
+      // ./mp4info frag_bunny.mp4 | grep Codec
+      var mimeCodec = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
+
+      if ('MediaSource' in window && MediaSource.isTypeSupported(mimeCodec)) {
+        var mediaSource = new MediaSource;
+        //console.log(mediaSource.readyState); // closed
+        video.src = URL.createObjectURL(mediaSource);
+        mediaSource.addEventListener('sourceopen', sourceOpen);
+      } else {
+        console.error('Unsupported MIME type or codec: ', mimeCodec);
+      }
+
+      function sourceOpen (_) {
+        //console.log(this.readyState); // open
+        var mediaSource = this;
+        var sourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
+        fetchAB(assetURL, function (buf) {
+          sourceBuffer.addEventListener('updateend', function (_) {
+            mediaSource.endOfStream();
+            video.play();
+            //console.log(mediaSource.readyState); // ended
+          });
+          sourceBuffer.appendBuffer(buf);
+        });
+      };
+
+      function fetchAB (url, cb) {
+        
+        var xhr = new XMLHttpRequest;
+        xhr.open('get', url);
+        xhr.responseType = 'arraybuffer';
+        xhr.onload = function () {
+          cb(xhr.response);
+        };
+        xhr.send();
+      };
+    // END
+    
     video.load();
     audio.load();
     video.oncanplay = function() {
@@ -66,3 +106,11 @@ var x = foo.videos[0];
   }
 );
   }
+
+  
+
+      
+
+      
+   
+
